da2e7cef872471e96b121d6dfa209b04c1a686be
baggio4ever <baggio4ever@gmail.com>
2018-02-10 11:54:39 +0900

    AngularロゴなどをAboutComponent に移動

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  title = 'tactics-board-app';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  getAngularVersion(): string {
+    return VERSION.full;
+  }
 }
